9c1b89a576fcbdb5b485b7b366d8313a64215f15
Prince-k6 <pr040306ku@gmail.com>
2026-07-12 12:26:10 +0530

    ADA

--- kruskals.c
+++ kruskals.c
@@ -2,9 +2,9 @@
 #include<stdlib.h>
 #include<stdbool.h>
 
+int n,cost[9][9],parent[9];
+int ne=1, minCost=0;     //no. of edges , minimum cost
 
-int n,cost[9][9],parent[9];         //no.of vertices, cost adj matrix, parent
-int ne = 1,minCost=0;               // no of edges in mst and minCost
 
 int find(int i){
     while(parent[i]!=-1){
@@ -12,19 +12,17 @@ int find(int i){
     }
     return i;
 }
-bool uni(int a, int b){
-    int u = find(a);      //find the parent of uth node
-    int v = find(b);      // find the parent of vth node
-    if(u != v){
+bool uni(int a,int b){
+    int u = find(a);
+    int v = find(b);
+    if(u!=v){
         parent[b] = a;
         return true;
     }
     return false;
 }
-
 int main(){
     n = 5;
-    //if cost[i][j] = 0 then make cost[i][j] = 999;
     int cost[5][5] = {
         {999, 2, 999, 6, 999},
         {2, 999, 3, 8, 5},
@@ -32,18 +30,19 @@ int main(){
         {6, 8, 999, 999, 9},
         {999, 5, 7, 9, 999}
     };
-    //sabka parent initially null hoga
-    for(int i=0; i<=n;i++){
+
+    //parent ko initialise with -1;
+    for(int i=0; i<n;i++){
         parent[i] = -1;
     }
 
     printf("The edges in a minimum spanning tree are:\n");
     while(ne < n){
-        //sabse pahle minimum cost/edge find krunga overall graph me
-        int min = 999;
-        int a,b;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+        int min =999;  //to store the cost on each iteration
+        int a,b;      //to store the edge on each iteration
+
+        for(int i=0; i<n ;i++){
+            for(int j=0;j<n;j++){
                 if(cost[i][j] < min){
                     min = cost[i][j];
                     a = i;
@@ -51,11 +50,11 @@ int main(){
                 }
             }
         }
-        if(uni(a,b)){      // if they doesn't belong to same component return true ans print the edge
+        if(uni(a,b)){
             printf("%d edge(%d,%d) = %d\n",ne++,a,b,min);
-            minCost += min;
+            minCost+=min;
         }
-        cost[a][b] = cost[b][a] = 999;     //so that same edge doesn't repeat
+        cost[a][b] = cost[b][a] = 999;    //so that it doesn't get included once again
     }
     printf("minimum cost of spanning tree : %d\n",minCost);
     return 0;
